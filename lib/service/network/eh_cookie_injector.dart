c6a9f6195a97276b797170f464edebb854609c6b
Errorsia <Errorsia@outlook.com>
2026-05-24 15:37:12 +0800

    framework

--- lib/service/network/eh_cookie_injector.dart
+++ lib/service/network/eh_cookie_injector.dart
@@ -3,7 +3,6 @@ import 'package:dio/dio.dart';
 import 'dart:io';
 import 'cookie_utils.dart';
 
-
 class EHCookieManager {
   late final List<Cookie> dynamicCookies;
   late final _EHCookieInjector _cookieInjector;
@@ -12,28 +11,58 @@ class EHCookieManager {
     // dynamicCookies will be loaded here
     dynamicCookies = [Cookie('x', 'x'), Cookie('datatags', '1')];
 
-    _cookieInjector = _EHCookieInjector(dynamicCookies);
+    _cookieInjector = _EHCookieInjector(dynamicCookies, _updateCookies);
   }
 
   Interceptor get cookieInjector => _cookieInjector;
 
-
+  // OK but not the best choice
   // Interceptor getCookieInjector() {
   //   return cookieInjector;
   // }
 
+  void _updateCookies(List<Cookie> newCookies) {
+    for (var c in newCookies) {
+      dynamicCookies.removeWhere((old) => old.name == c.name);
+      dynamicCookies.add(c);
+    }
+    //  todo: store cookies
+  }
+
+  // Timer? _saveTimer;
+
+  // void _updateCookies(List<Cookie> newCookies) {
+  //   // merge
+  //   for (var c in newCookies) {
+  //     dynamicCookies.removeWhere((old) => old.name == c.name);
+  //     dynamicCookies.add(c);
+  //   }
+
+  //   // delay in writing
+  //   _saveTimer?.cancel();
+  //   _saveTimer = Timer(Duration(seconds: 1), () {
+  //     saveToDisk(dynamicCookies);
+  //   });
+  // }
+
 }
 
-
 class _EHCookieInjector extends Interceptor {
-  final List<Cookie> immutableCookies = [Cookie('nw', '1'), Cookie('datatags', '1')];
+  final void Function(List<Cookie>) onCookiesReceived;
+  final List<Cookie> immutableCookies = [
+    Cookie('nw', '1'),
+    Cookie('datatags', '1'),
+  ];
 
   final List<Cookie> dynamicCookies;
 
-  _EHCookieInjector(this.dynamicCookies);
+  _EHCookieInjector(
+    // this.immutableCookies,
+    this.dynamicCookies,
+    this.onCookiesReceived,
+  );
 
   List<Cookie> get cookies => [...immutableCookies, ...dynamicCookies];
-
 
   Future<void> initCookies() async {
     // String? string = await localConfigService.read(
@@ -48,12 +77,11 @@ class _EHCookieInjector extends Interceptor {
   }
 
   @override
-  void onRequest(RequestOptions options, RequestInterceptorHandler handler) {    
+  void onRequest(RequestOptions options, RequestInterceptorHandler handler) {
     try {
       if (networkSetting.allHostAndIPs.contains(options.uri.host)) {
-        options.headers[HttpHeaders.cookieHeader] = CookieUtils.safeCookieString(
-          cookies,
-        );
+        options.headers[HttpHeaders.cookieHeader] =
+            CookieUtils.safeCookieString(cookies);
       }
       handler.next(options);
     } on Exception catch (e, stackTrace) {
@@ -68,17 +96,11 @@ class _EHCookieInjector extends Interceptor {
 
   @override
   void onResponse(Response response, ResponseInterceptorHandler handler) {
-    try {
-      _saveEHCookies(response);
-      handler.next(response);
-    } on Exception catch (e, s) {
-      final err = DioException(
-        requestOptions: response.requestOptions,
-        error: e,
-        stackTrace: s,
-      );
-      return handler.reject(err, true);
+    final setCookies = response.headers['set-cookie'];
+    if (setCookies != null) {
+      final parsed = setCookies.map(Cookie.fromSetCookieValue).toList();
+      onCookiesReceived(parsed);
     }
+    handler.next(response);
   }
-
 }
