cf2814dfbe92dc82fe265fa89f4c458009604f95
Errorsia <Errorsia@outlook.com>
2026-05-24 15:08:30 +0800

    cookie manager framework

--- lib/service/network/eh_cookie_injector.dart
+++ lib/service/network/eh_cookie_injector.dart
@@ -5,37 +5,50 @@ import 'cookie_utils.dart';
 
 
 class EHCookieManager {
-  final EHCookieInjector cookieInjector = EHCookieInjector();
+  late final List<Cookie> dynamicCookies;
+  late final _EHCookieInjector _cookieInjector;
 
-  Future<void> replaceCookies(List<Cookie> cookies) async {
-    
+  EHCookieManager() {
+    // dynamicCookies will be loaded here
+    dynamicCookies = [Cookie('x', 'x'), Cookie('datatags', '1')];
+
+    _cookieInjector = _EHCookieInjector(dynamicCookies);
   }
 
+  Interceptor get cookieInjector => _cookieInjector;
 
-  EHCookieInjector getCookieInjector(){
-    return cookieInjector;
-  }
+
+  // Interceptor getCookieInjector() {
+  //   return cookieInjector;
+  // }
 
 }
 
-class EHCookieInjector extends Interceptor {
+
+class _EHCookieInjector extends Interceptor {
   final List<Cookie> immutableCookies = [Cookie('nw', '1'), Cookie('datatags', '1')];
+
+  final List<Cookie> dynamicCookies;
+
+  _EHCookieInjector(this.dynamicCookies);
+
+  List<Cookie> get cookies => [...immutableCookies, ...dynamicCookies];
 
 
   Future<void> initCookies() async {
-    String? string = await localConfigService.read(
-      configKey: ConfigEnum.ehCookie,
-    );
-    if (string != null) {
-      List list = jsonDecode(string);
-      cookies.addAll(
-        list.cast<String>().map(Cookie.fromSetCookieValue).toList(),
-      );
-    }
+    // String? string = await localConfigService.read(
+    //   configKey: ConfigEnum.ehCookie,
+    // );
+    // if (string != null) {
+    //   List list = jsonDecode(string);
+    //   cookies.addAll(
+    //     list.cast<String>().map(Cookie.fromSetCookieValue).toList(),
+    //   );
+    // }
   }
 
   @override
-  void onRequest(RequestOptions options, RequestInterceptorHandler handler) {
+  void onRequest(RequestOptions options, RequestInterceptorHandler handler) {    
     try {
       if (networkSetting.allHostAndIPs.contains(options.uri.host)) {
         options.headers[HttpHeaders.cookieHeader] = CookieUtils.safeCookieString(
